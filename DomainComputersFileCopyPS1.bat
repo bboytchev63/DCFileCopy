c1668248fdd47d67692c4b2ecf843afa0dbbcb6b
bboytchev63 <bboytchev@hotmail.com>
2025-04-03 11:56:40 +0300

    new1

--- DomainComputersFileCopyPS1.bat
+++ DomainComputersFileCopyPS1.bat
@@ -3,5 +3,5 @@ rem [1] $BackupDestinationRoot
 rem [2] $backupFileExt    // *.doc? *.xls? *.ppt? ... 
 rem [3] $ou1   // DELOVODSTVO  ,  secretaries , staff
 
-powershell .\DomainComputersFileCopyPS.ps1 "d$\" "J:\Projects\powershell\data\" "*.ppt?" "staff"
+powershell .\DomainComputersFileCopyPS.ps1 "d$\" "J:\Projects\powershell\data\" "*.doc?,*.ppt?,*.xls?" "staff"
 pause   
