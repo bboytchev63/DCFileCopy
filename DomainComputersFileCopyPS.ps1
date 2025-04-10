3aceef4f06b1bdd6dcfa8c68d9ace2bd8521161b
bboytchev63 <bboytchev@hotmail.com>
2025-04-10 11:42:43 +0300

    error

--- DomainComputersFileCopyPS.ps1
+++ DomainComputersFileCopyPS.ps1
@@ -9,7 +9,7 @@ param (
     [parameter(Position = 1)]
     [string]$BackupDestinationRoot = "J:\Projects\powershell\data", 
     [parameter(Position = 2)]
-    [string]$backupFileExt = "*.xls?"  ,  # "*.doc? *.ppt? *.xls?",
+    [string]$backupFileExt = "*.doc?"  ,  # "*.doc? *.ppt? *.xls?",
     [parameter(Position = 3)]
     [string]$ou_1 = "staff"  
 )
